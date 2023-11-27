583b374f2995a407ed82712ccd8c407ed3621da8
Syahrul Rizki <62525690+syahrulrzk@users.noreply.github.com>
2023-11-27 12:37:40 +0700

    Update build_script.py

--- build_script.py
+++ build_script.py
@@ -1,9 +1,6 @@
 # build_script.py
-
 import subprocess
 import sys
-import os
-import shutil
 
 def install_dependencies():
     try:
@@ -18,34 +15,12 @@ def install_dependencies():
 def build_project():
     try:
         print("Building the Python project...")
-
-        # Check if 'build' directory already exists
-        if not os.path.exists("build"):
-            os.makedirs("build")
-        else:
-            print("'build' directory already exists. Removing its contents.")
-            # Clear the contents of the 'build' directory if it already exists
-            shutil.rmtree("build")
-            os.makedirs("build")
-
-        # Copy or move the build artifacts to the 'build' directory
-        for item in os.listdir("."):
-            if item != "build":  # Exclude the 'build' directory
-                src_path = item
-                dest_path = os.path.join("build", item)
-                if os.path.isdir(item):
-                    shutil.copytree(src_path, dest_path)
-                else:
-                    shutil.copy(src_path, dest_path)
-
+        # Add additional build steps as needed
         print("Build completed successfully!")
-    except subprocess.CalledProcessError as e:
+    except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
 
-def main():
+if __name__ == "__main__":
     install_dependencies()
     build_project()
-
-if __name__ == "__main__":
-    main()
