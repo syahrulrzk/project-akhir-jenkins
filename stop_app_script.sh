621c703fe8d1fcd321b9a4609fb81931c03fb93d
Syahrul Rizki <62525690+syahrulrzk@users.noreply.github.com>
2023-12-04 09:05:02 +0700

    Create stop_app_script.sh

---
+++ stop_app_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script untuk menghentikan aplikasi
+echo "Stopping the application..."
+
+# Tambahkan perintah atau langkah-langkah untuk menghentikan aplikasi
+# Misalnya:
+# pm2 stop myapp (contoh untuk aplikasi Node.js menggunakan pm2)
+
+echo "Application stopped successfully."
